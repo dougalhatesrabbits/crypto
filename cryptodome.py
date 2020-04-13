18d962b1fc9f029926f2005899b8477d50cb9eab
MightyBoosh <david.brooke14@icloud.com>
2020-04-13 02:56:03 +0100

    1

--- cryptodome.py
+++ cryptodome.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+
 
 from Crypto import Random  # use to generate a random byte string of a length we decide
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256
-from Crypto import Random
+from Cryptodome.Cipher import AES
+from Cryptodome.Hash import SHA256
+from Cryptodome import Random
+from Cryptodome.Random import get_random_bytes
 
 # Builtins
 import base64
@@ -105,3 +106,37 @@ print("\nenc:  {}".format(encrypted))
 decrypted = decrypt(my_password, encrypted)
 print("dec:  {}".format(decrypted))
 print("\ndata match: {}".format(my_data == decrypted))
+
+"""
+https://www.pycryptodome.org/en/latest/src/examples.html
+"""
+'''
+Encrypt data with AES
+
+The following code generates a new AES128 key and encrypts a piece of data into a file. 
+We use the EAX mode because it allows the receiver to detect any unauthorized modification (similarly, 
+we could have used other authenticated encryption modes like GCM, CCM or SIV).
+
+'''
+data = "hello"
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+
+'''
+At the other end, the receiver can securely load the piece of data back (if they know the key!). 
+Note that the code generates a ValueError exception when tampering is detected.
+
+'''
+
+from Crypto.Cipher import AES
+
+file_in = open("encrypted.bin", "rb")
+nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+
+# let's assume that the key is somehow available again
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+data = cipher.decrypt_and_verify(ciphertext, tag)
